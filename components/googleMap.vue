118d9e9cdb73ac3da6ddc33eb2c6d17309dc96d2
Samuel Austen <furneauxsam1994@gmail.com>
2020-04-13 13:47:02 +0200

    Google map component

---
+++ components/googleMap.vue
@@ -0,0 +1,19 @@
+<template>
+	<div>
+		<iframe
+			width="100%"
+			height="500"
+			:src="'https://maps.google.com/maps?width=100%&amp;height=900&amp;hl=en&amp;q='+props+'()&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed'"
+			frameborder="0"
+			scrolling="no"
+			marginheight="0"
+			marginwidth="0"
+		></iframe>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["props"]
+};
+</script>
