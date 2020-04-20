b28f01b447905507ef0c64f815b6a1f0eafbbaf1
Goldenfaeron <tobias@mreich.de>
2020-04-20 18:53:00 +0200

    components

---
+++ components/fromTemplate/welcome.vue
@@ -0,0 +1,35 @@
+
+<template>
+	<section id="hero">
+		<v-row no-gutters>
+			<v-img
+				:min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+				:src="'/img/'+$store.state.city +'2.jpg'"
+			>
+				<v-theme-provider dark>
+					<v-container fill-height>
+						<v-row align="center" class="white--text mx-auto" justify="center">
+							<v-col class="white--text text-center" cols="12" tag="h1">
+								<span
+									class="font-weight-light"
+									:class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+								>WELCOME TO</span>
+
+								<br />
+
+								<span
+									:class="[$vuetify.breakpoint.smAndDown ? 'display-3': 'display-4']"
+									class="font-weight-black"
+								>BORSH</span>
+							</v-col>
+
+							<v-btn class="align-self-end" fab outlined @click="$vuetify.goTo('#about')">
+								<v-icon>mdi-chevron-double-down</v-icon>
+							</v-btn>
+						</v-row>
+					</v-container>
+				</v-theme-provider>
+			</v-img>
+		</v-row>
+	</section>
+</template>
---
+++ pages/test.vue
@@ -0,0 +1,31 @@
+<template>
+<v-content>
+	<welcome></welcome>
+	<about></about>
+	<features></features>
+	<stats></stats>
+	<blog></blog>
+	<contact></contact>
+</v-content>
+</template>
+
+
+<script>
+import welcome from "~/components/fromTemplate/welcome.vue";
+import about from "~/components/fromTemplate/about.vue";
+import features from "~/components/fromTemplate/features.vue";
+import stats from "~/components/fromTemplate/stats.vue";
+import blog from "~/components/fromTemplate/blog.vue";
+import contact from "~/components/fromTemplate/contact.vue";
+
+export default {
+	components: {
+		welcome,
+		about,
+		features,
+		stats,
+		blog,
+		contact,
+	}
+};
+</script>
