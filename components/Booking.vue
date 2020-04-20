f0b85ccbc0eddf1bc2f68482fd554b4b07f8eb78
Samuel Austen <furneauxsam1994@gmail.com>
2020-04-20 20:57:42 +0200

    components Booking.vue, Instagram.vue - saved old homepage

---
+++ components/Booking.vue
@@ -0,0 +1,65 @@
+		<template>
+	<v-container grid-list-lg>
+		<h1>{{$store.state.city}} HOTELS</h1>
+		<v-responsive class="mx-auto mb-12" width="56">
+			<v-divider></v-divider>
+		</v-responsive>
+		<v-layout row wrap mt-5>
+			<v-container grid-list-lg>
+				<v-layout row wrap>
+					<v-flex
+						xs12
+						sm12
+						md6
+						lg3
+						v-for="(item, index) in $store.state.borshHotels[0].slice(0,4) "
+						:key="index"
+					>
+						<v-card>
+							<!-- <v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img> -->
+
+							<v-card-title primary-title>
+								<v-layout align-content-space-between justify-space-between>
+									<v-flex>
+										<p class="headline d-flex">{{item.name }}</p>
+									</v-flex>
+								</v-layout>
+							</v-card-title>
+
+							<v-img max-height="200" :src="item.image"></v-img>
+
+							<v-card-text>
+								<br />
+								From {{item.price}} Euro per night
+							</v-card-text>
+
+							<div class="text-xs-center">
+								<v-rating color="red" :value="item.rating/2" half-increments readonly></v-rating>
+							</div>
+
+							<!-- v-if="!item.inCart" -->
+							<v-card-actions>
+								<!-- <v-btn color="secondary"> -->
+								<!-- <nuxt-link :to="'/booking/'+index" class="accent--text">Read more</nuxt-link> -->
+								<!-- </v-btn> -->
+							</v-card-actions>
+						</v-card>
+					</v-flex>
+				</v-layout>
+			</v-container>
+		</v-layout>
+	</v-container>
+</template>
+
+</script>
+
+<style lang="css" scoped>
+h1,
+h2,
+h3,
+h4,
+h5,
+p {
+	text-align: center;
+}
+</style>
---
+++ components/Instagram.vue
@@ -0,0 +1,55 @@
+	<template>
+	<div>
+		<h1>{{$store.state.city}} ON INSTAGRAM</h1>
+		<v-responsive class="mx-auto mb-12" width="56">
+			<v-divider></v-divider>
+		</v-responsive>
+		<v-container grid-list-lg mt-5>
+			<v-layout row wrap>
+				<!-- <v-card> -->
+				<v-container grid-list-lg>
+					<v-layout row wrap>
+						<v-flex
+							xs12
+							sm12
+							md6
+							lg3
+							v-for="(item, index) in $store.state.borshInstagram[0].slice(0,4) "
+							:key="index"
+						>
+							<v-card>
+								<!-- <v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img> -->
+
+								<v-img max-height="200" :src="item.imageUrl"></v-img>
+
+								<v-card-text>
+									{{item.firstComment}}
+									<br />
+									<v-icon>mdi-cards-heart</v-icon>
+									{{item.likesCount}}
+								</v-card-text>
+
+								<!-- v-if="!item.inCart" -->
+								<v-card-actions></v-card-actions>
+							</v-card>
+						</v-flex>
+					</v-layout>
+				</v-container>
+				<!-- </v-card> -->
+			</v-layout>
+		</v-container>
+	</div>
+</template>
+
+</script>
+
+<style lang="css" scoped>
+h1,
+h2,
+h3,
+h4,
+h5,
+p {
+	text-align: center;
+}
+</style>
