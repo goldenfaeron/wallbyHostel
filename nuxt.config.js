873e71196cbe316a65b0525ba28aa1c07eccc6d1
Samuel Austen <furneauxsam1994@gmail.com>
2020-04-24 14:11:19 +0200

    local mobile hosting

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,11 @@ export default {
       }
     }
   },
+
+  server: {
+    port: 8000, // default: 3000     
+    host: '0.0.0.0', // default: localhost   
+  },
   /*
   ** Build configuration
   */
