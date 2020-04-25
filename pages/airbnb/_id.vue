5a6cb4f1386d1b740db73a259efec656344371e0
Goldenfaeron <tobias@mreich.de>
2020-04-25 20:06:28 +0200

    no message

--- pages/airbnb/_id.vue
+++ pages/airbnb/_id.vue
@@ -1,41 +1,108 @@
 <template>
 	<div>
-		<v-parallax :src="data.image"></v-parallax>
+		<v-carousel>
+			<v-carousel-item v-for="(item, index) in data.photos" :key="index" :src="item.large"></v-carousel-item>
+		</v-carousel>
+
 		<v-container grid-list-xs>
-			<v-layout row wrap>
-				<v-flex xs12>
-					<!-- <v-img :src="data.image"></v-img> -->
-					<h1>{{data.name}}</h1>
-					<h3>{{data.type}}</h3>
-					<p>{{data.description}}</p>
-					<v-divider></v-divider>
-					Reviews {{data.reviews}}
-					<v-divider></v-divider>
-					Breakfast: {{data.breakfast}}
-					<v-divider></v-divider>
-					Check in from: {{data.checkInFrom}} until {{data.checkInTo}}
-					<v-divider></v-divider>
-					Address: {{data.address.full}}
-				</v-flex>
-				<v-flex xs12>
-					<h2>Rooms</h2>
-					<v-container grid-list-lg>
+			<span class="grey--text text--darken-3">
+				<v-layout row justify-space-between>
+					<v-flex lg5>
+						<p class="display-1 bold font-weight-bold" color>{{data.name}}</p>
+						<p>{{data.address}}</p>
+						<br />
 						<v-layout row wrap>
-							<v-flex xs12 sm6 md3 v-for="(item, index) in data.rooms" :key="index">
-								<v-card>
-									<v-card-title primary-title>{{item.roomType}}</v-card-title>
-									<v-card-title primary-title>EURO {{item.price}}</v-card-title>
-									<v-card-text>
-										{{item.bedType}}
-										<br />
-										Persons {{item.persons}}
-									</v-card-text>
-								</v-card>
+							<v-flex lg3>
+								<p>{{data.numberOfGuests}} guests</p>
+							</v-flex>
+							<v-flex lg3>
+								<p>{{data.bedroomLabel}}</p>
+							</v-flex>
+							<v-flex lg3>
+								<p>{{data.bedLabel}}</p>
+							</v-flex>
+							<v-flex lg3>
+								<p>{{data.bathroomLabel}}</p>
 							</v-flex>
 						</v-layout>
-					</v-container>
-				</v-flex>
-			</v-layout>
+						<br />
+						<v-divider></v-divider>
+						<br />
+						<span v-for="(item, index) in data.highlights" :key="index">
+							<p>
+								<v-icon color="green">mdi-check</v-icon>
+								<span class="font-weight-medium">{{item.headline}}</span>
+								<v-spacer></v-spacer>
+								{{item.message}}
+							</p>
+						</span>
+						<br />
+						<v-divider></v-divider>
+						<br />
+						<p class="font-weight-medium">Amneties</p>
+						<v-layout row wrap>
+							<v-flex lg6 xs12 v-for="(item, index) in data.listingAmenities.slice(-6)" :key="index">
+								<p v-if="item.isPresent == true">
+									<v-icon color="green">mdi-check</v-icon>
+									{{item.name}}
+								</p>
+								<p v-else>
+									<v-icon color="red">mdi-close</v-icon>
+									{{item.name}}
+								</p>
+							</v-flex>
+							<v-dialog v-model="dialog" width="500" scrollable>
+								<template v-slot:activator="{ on }">
+									<v-layout justify-center>
+										<v-btn color="primary" dark v-on="on" text>View all amneties</v-btn>
+									</v-layout>
+								</template>
+
+								<v-card>
+									<v-card-title class="headline grey lighten-2" primary-title>
+										Amneties
+										<v-spacer></v-spacer>
+										<v-btn flat @click="dialog = false" icon>
+											<v-icon>mdi-close</v-icon>
+										</v-btn>
+									</v-card-title>
+
+									<v-card-text>
+										<br />
+										<v-flex v-for="(item, index) in data.listingAmenities" :key="index">
+											<p v-if="item.isPresent == true">
+												<v-icon color="green">mdi-check</v-icon>
+												{{item.name}}
+											</p>
+											<p v-else>
+												<v-icon color="red">mdi-close</v-icon>
+												{{item.name}}
+											</p>
+										</v-flex>
+									</v-card-text>
+
+									<v-divider></v-divider>
+								</v-card>
+							</v-dialog>
+						</v-layout>
+						<br />
+						<v-divider></v-divider>
+						<br />
+						<p class="font-weight-medium">Sleeping arrangements</p>
+
+						<ul>
+							<li v-for="(item, index) in data.listingRooms" :key="index">
+								<p id="sleep">{{item.beds[0].quantity}} x {{item.beds[0].type}}</p>
+							</li>
+						</ul>
+					</v-flex>
+					<v-flex lg5>
+						<p class="display-1 bold font-weight-bold">Description</p>
+						<br />
+						<pre style="white-space: pre-wrap;">{{data.sectionedDescription.description}}</pre>
+					</v-flex>
+				</v-layout>
+			</span>
 		</v-container>
 	</div>
 </template>
@@ -44,8 +111,15 @@
 export default {
 	data() {
 		return {
-			data: this.$store.state.borshHotels[0][this.$route.params.id]
+			data: this.$store.state.borshAirbnb[0][this.$route.params.id],
+			dialog: false
 		};
+	},
+	filters: {
+		truncate(string, value) {
+			return (string || "").substring(0, value);
+		}
 	}
 };
 </script>
+
--- pages/airbnb/index.vue
+++ pages/airbnb/index.vue
@@ -1,41 +1,36 @@
 <template>
 	<div>
-		<h1 class="primary--text main-title" style="text-align: center;">Hotels in Borsh</h1>
+		<h1 class="primary--text main-title" style="text-align: center;">Vacation in Borsh</h1>
 		<!-- <h1>{{$store.state.borshHotels[0][0].order}}</h1> -->
 		<!-- {{$store.state.borshHotels}} -->
 		<v-container grid-list-lg>
 			<v-layout row wrap>
-				<v-flex xs12 sm12 md6 lg3 v-for="(item, index) in $store.state.borshAirbnb[0]" :key="index">
-					<v-card>
-						<!-- <v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img> -->
-						{{item}}
-						<!-- <v-card-title primary-title>
+				<v-flex xs12 sm12 md6 lg4 v-for="(item, index) in $store.state.borshAirbnb[0]" :key="index">
+					<v-card style="height: 100%;">
+						<v-card-title primary-title>
 							<v-layout align-content-space-between justify-space-between>
 								<v-flex>
 									<p class="headline d-flex">{{item.name }}</p>
 								</v-flex>
 							</v-layout>
 						</v-card-title>
-
-						<v-img :src="item.image"></v-img>
-
+						<v-img v-if="item.photos" max-height="200" :src="item.photos[0].large"></v-img>
+						<v-img v-else max-height="200" :src="'/img/placeholder'+placeholder(index)+'.svg'"></v-img>
 						<v-card-text>
-							{{item.description}}
+							<p style="font-weight: bold;" class="truncate-overflow">{{item.sectionedDescription.description}}</p>
 							<br />
-							{{item.price}}
+							Room Type: {{item.roomType}}
+							<br />
+							<v-icon>mdi-square-edit-outline</v-icon>
+							Read {{item.reviews.length}} reviews
 						</v-card-text>
-						{{item.rating /2}}
 						<div class="text-xs-center">
-							<v-rating color="red" :value="item.rating/2" half-increments readonly></v-rating>
+							<v-rating color="red" :value="item.stars" half-increments readonly></v-rating>
 						</div>
 
-						<!-- v-if="!item.inCart"-->
-						<v-card-actions>
-							<v-btn color="secondary">
-								<nuxt-link :to="'/booking/'+index" class="accent--text">Read more</nuxt-link>
-							</v-btn>
-							<v-btn color="success">Read Reviews</v-btn>
-						</v-card-actions>-->
+						<v-btn color="primary" ><nuxt-link :to="'/airbnb/'+index" class="accent--text">Read more</nuxt-link></v-btn>
+						<v-btn color="success" :href="'https://airbnb.com/rooms/'+item.id">book on airbnb.com</v-btn>
+						
 					</v-card>
 				</v-flex>
 			</v-layout>
@@ -44,5 +39,41 @@
 </template>
 
 <script>
-export default {};
+export default {
+	methods: {
+		placeholder(index) {
+			return index % 2;
+		}
+	},
+	filters: {
+		truncate(string, value) {
+			return (string || "").substring(0, value);
+		}
+	},
+};
 </script>
+<style > 
+.truncate-overflow {
+	--lh: 1.4rem;
+	line-height: var(--lh);
+	--max-lines: 3;
+	position: relative;
+	height: calc(var(--lh) * var(--max-lines));
+	overflow: hidden;
+	padding-right: 1rem; /* space for ellipsis */
+}
+.truncate-overflow::before {
+	position: absolute;
+	content: "...";
+	inset-block-end: 0; /* "bottom" */
+	inset-inline-end: 0; /* "right" */
+}
+.truncate-overflow::after {
+	content: "";
+	position: absolute;
+	inset-inline-end: 0; /* "right" */
+	width: 1rem;
+	height: 1rem;
+	background: white;
+}
+</style>
