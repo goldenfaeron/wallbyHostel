749b3d303dfbce1a1aeb5748c78da7d1fefc9904
Goldenfaeron <tobias@mreich.de>
2020-04-29 19:46:04 +0200

    finished new instagram page for desktop the first see more button is working first hashtags are fading in you can change the fade in time in style

--- components/insta/Welcome.vue
+++ components/insta/Welcome.vue
@@ -8,9 +8,10 @@
 			<p class="display-3 font-weight-medium">Post on Goborshi with your instagram</p>
 			<v-flex lg10>
 				<p
-					class="subheading font-weight-medium"
+					class="headline"
 				>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
 			</v-flex>
+			<br />
 			<div class="display-1 font-weight-medium fade-in">
 				<p>#barilliri</p>
 				<p>#sumukelele</p>
@@ -47,6 +48,50 @@
 				</v-flex>
 			</v-layout>
 		</v-container>
+		<v-layout row justify-center>
+			<v-dialog v-model="dialog" fullscreen hide-overlay transition="dialog-bottom-transition">
+				<template v-slot:activator="{ on }">
+					<v-btn color="primary" dark v-on="on">See more</v-btn>
+				</template>
+				<v-card>
+					<v-toolbar dark color="primary">
+						<v-btn icon dark @click="dialog = false">
+							<v-icon>mdi-close</v-icon>
+						</v-btn>
+						<v-toolbar-title>{{$store.state.city}} Instagram</v-toolbar-title>
+					</v-toolbar>
+					<v-container grid-list-lg>
+						<!-- <iframe width="420" height="315" src="https://www.youtube.com/embed/HWj0XkS091g?autoplay=1"></iframe> -->
+						<v-layout row wrap>
+							<v-flex
+								xs12
+								sm12
+								md6
+								lg3
+								v-for="(item, index) in $store.state.borshInstagram[0]"
+								:key="index"
+							>
+								<v-card height="100%">
+									<!-- <v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img> -->
+
+									<v-img height="200px" :src="item.imageUrl"></v-img>
+
+									<v-card-text>
+										{{item.firstComment}}
+										<br />
+										<v-icon color="red">mdi-cards-heart</v-icon>
+										{{item.likesCount}}
+									</v-card-text>
+
+									<!-- v-if="!item.inCart" -->
+									<v-card-actions></v-card-actions>
+								</v-card>
+							</v-flex>
+						</v-layout>
+					</v-container>
+				</v-card>
+			</v-dialog>
+		</v-layout>
 		<br />
 		<p align="center" class="display-3 font-weight-medium">Promote your Instagram content</p>
 		<v-container grid-list-lg>
@@ -82,6 +127,8 @@
 			</v-layout>
 		</v-container>
 		<br />
+
+		<!--BORSH INSTAGRAM-->
 		<v-row>
 			<v-img
 				height="760"
@@ -92,7 +139,7 @@
 						<v-col class="white--text text-center" cols="12" tag="h1">
 							<v-layout justify-center>
 								<v-card color="grey" flat>
-									<v-card-title primary-title>
+									<v-card-title primary-title style="word-break: normal;">
 										<span
 											class="font-weight-light font-weight-medium"
 											:class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
@@ -120,9 +167,37 @@
 					<div align="center" class="display-1 font-weight-bold">
 						<p>Get morefollowers and post your content instantly</p>
 					</div>
+					<br />
+					<v-layout row wrap>
+						<v-flex lg4>
+							<div align="center" class="headline">
+								<v-img src="/img/phoneHearth.png"></v-img>
+								<br />
+								<p>#ex ea commodo</p>
+								<p>#consequad</p>
+								<p>#Duis aute</p>
+								<p>#irure</p>
+							</div>
+						</v-flex>
+						<v-flex lg4>
+							<v-img height="80%" src="/img/phoneGraph.png"></v-img>
+						</v-flex>
+						<v-flex lg4>
+							<div align="center" class="headline">
+								<v-img src="/img/phoneHearth.png"></v-img>
+								<br />
+								<p>#ex ea commodo</p>
+								<p>#consequad</p>
+								<p>#Duis aute</p>
+								<p>#irure</p>
+							</div>
+						</v-flex>
+					</v-layout>
 				</v-flex>
 			</v-layout>
 		</v-container>
+
+		<!--GOBORSHI APP-->
 		<v-row>
 			<br />
 			<v-img
@@ -134,11 +209,11 @@
 						<v-col class="white--text text-center" cols="12" tag="h1">
 							<v-layout justify-center>
 								<v-card color="grey" flat>
-									<v-card-title primary-title>
+									<v-card-title primary-title style="word-break: normal;">
 										<span
 											class="font-weight-light font-weight-medium"
 											:class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
-										>Borsh Instagram</span>
+										>GoBorshi App</span>
 									</v-card-title>
 								</v-card>
 							</v-layout>
@@ -151,31 +226,177 @@
 		<v-container grid-list-lg>
 			<v-layout row wrap>
 				<v-flex lg4>
-					<v-layout justify-center>
-						<v-icon size="400">mdi-cellphone</v-icon>
-					</v-layout>
+					<div justify-center>
+						<v-img src="/img/phoneStar.png"></v-img>
+					</div>
 				</v-flex>
 				<v-flex lg4>
-					<v-layout justify-center>
-						<v-icon size="550">mdi-cellphone</v-icon>
-					</v-layout>
+					<div justify-center>
+						<v-img src="/img/phoneImg.png"></v-img>
+					</div>
 				</v-flex>
 				<v-flex lg4>
-					<v-layout justify-center>
-						<v-icon size="400">mdi-cellphone</v-icon>
-					</v-layout>
+					<div justify-center>
+						<v-img src="/img/phoneWww.png"></v-img>
+					</div>
 				</v-flex>
 			</v-layout>
 			<br />
+			<div align="center" class="display-1 font-weight-bold">
+				<p>Share videos, locations and hangouts on Goborshi</p>
+			</div>
+			<div align="center" class="headline">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+			</div>
+			<br />
 			<v-layout justify-center>
-				<v-btn color="success">see more</v-btn>
+				<v-btn color="primary">see more</v-btn>
 			</v-layout>
 		</v-container>
+		<!--BORSH RESTAURANTS-->
+		<v-row>
+			<br />
+			<v-img
+				height="760"
+				src="https://scontent-lax3-2.cdninstagram.com/v/t51.2885-15/e35/34726024_479116625858790_6962504533595389952_n.jpg?_nc_ht=scontent-lax3-2.cdninstagram.com&_nc_cat=106&_nc_ohc=OeCb-yoFq_kAX88VbTr&oh=ceb2303a23cf563a4f002d831cf3abea&oe=5EBABCE8"
+			>
+				<v-container fill-height>
+					<v-row align="center" class="white--text mx-auto" justify="center">
+						<v-col class="white--text text-center" cols="12" tag="h1">
+							<v-layout justify-center>
+								<v-card color="grey" flat>
+									<v-card-title primary-title style="word-break: normal;">
+										<span
+											class="font-weight-light font-weight-medium"
+											:class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+										>Borsh Restaurants</span>
+									</v-card-title>
+								</v-card>
+							</v-layout>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-img>
+		</v-row>
+		<br />
+		<v-container grid-list-lg>
+			<v-layout row wrap>
+				<v-flex lg6>
+					<div align="center" class="display-1 font-weight-bold">
+						<p>Show and Promote your menu and products</p>
+						<br />
+						<v-img src="/img/menu.png"></v-img>
+					</div>
+				</v-flex>
+				<v-flex lg6>
+					<div align="center" class="display-1 font-weight-bold">
+						<p>Get more web presence to get more customers</p>
+						<br />
+						<v-img src="/img/webPresence.png"></v-img>
+					</div>
+				</v-flex>
+			</v-layout>
+			<br />
+			<p
+				align="center"
+				class="display-1 font-weight-bold"
+			>Show people on the web what your business has to offer</p>
+			<br />
+			<p
+				align="center"
+				class="headline"
+			>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+			<br />
+			<v-layout row wrap>
+				<v-flex lg4>
+					<v-img src="/img/mapsLogo.png"></v-img>
+				</v-flex>
+				<v-flex lg4>
+					<v-img src="/img/phoneMaps1.png"></v-img>
+				</v-flex>
+				<v-flex lg4>
+					<v-img src="/img/phoneMaps2.png"></v-img>
+				</v-flex>
+			</v-layout>
+			<br />
+			<p
+				align="center"
+				class="display-1 font-weight-bold"
+			>Your Google Maps listing will automatically appear on Goborshi</p>
+			<div align="center">
+				<v-btn color="primary">See more</v-btn>
+			</div>
+		</v-container>
+
+		<v-row>
+			<br />
+			<v-img height="760" src="/img/Beach-Party.jpg">
+				<v-container fill-height>
+					<v-row align="center" class="white--text mx-auto" justify="center">
+						<v-col class="white--text text-center" cols="12" tag="h1">
+							<v-layout justify-center>
+								<v-flex lg6 xs10>
+									<v-card color="grey" flat>
+										<v-card-title primary-title style="word-break: normal;">
+											<span
+												class="font-weight-light font-weight-medium"
+												:class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2']"
+											>
+												<p>
+													Give your business
+													a proper web presence with
+													Go App
+												</p>
+											</span>
+										</v-card-title>
+									</v-card>
+								</v-flex>
+							</v-layout>
+						</v-col>
+					</v-row>
+				</v-container>
+			</v-img>
+		</v-row>
+		<v-container grid-list-lg>
+			<v-layout row wrap>
+				<v-flex lg4>
+					<div align="center" class="headline">
+						<v-img src="/img/phoneGuestHouse1.png"></v-img>
+						<br />
+						<p>Get your own Beautiful Website</p>
+					</div>
+				</v-flex>
+				<v-flex lg4>
+					<div align="center" class="headline">
+						<v-img src="/img/phoneGuestHouse2.png"></v-img>
+						<br />
+						<p>Manage your content on Goborshi for free</p>
+					</div>
+				</v-flex>
+				<v-flex lg4>
+					<div align="center" class="headline">
+						<v-img src="/img/phoneGuestHouse3.png"></v-img>
+						<br />
+						<p>Find professional photographers and marketing professionals</p>
+					</div>
+				</v-flex>
+			</v-layout>
+		</v-container>
+		<div align="center">
+			<v-icon color="black" size="100">mdi-account</v-icon>
+			<br />
+			<v-btn color="primary">Start here</v-btn>
+		</div>
 	</div>
 </template>
 <script>
 export default {
-	components: {}
+	components: {},
+	data() {
+		return {
+			dialog: false
+		};
+	}
 };
 </script>
 <style lang="css">
--- pages/instagram/index.vue
+++ pages/instagram/index.vue
@@ -4,12 +4,12 @@
 		<welcome></welcome>
 		<!-- <h1>{{$store.state.borshHotels[0][0].order}}</h1> -->
 		<!-- {{$store.state.borshHotels}} -->
-		<v-container grid-list-lg>
-			<!-- <iframe width="420" height="315" src="https://www.youtube.com/embed/HWj0XkS091g?autoplay=1"></iframe> -->
+		<!--<v-container grid-list-lg>
+			 <iframe width="420" height="315" src="https://www.youtube.com/embed/HWj0XkS091g?autoplay=1"></iframe> 
 			<v-layout row wrap>
 				<v-flex xs12 sm12 md6 lg3 v-for="(item, index) in $store.state.borshInstagram[0]" :key="index">
 					<v-card height="100%">
-						<!-- <v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img> -->
+						 <v-img :src="$store.state.assetRoot + item.image.path" max-height="10cm"></v-img> 
 
 						<v-img height="200px" :src="item.imageUrl"></v-img>
 
@@ -20,12 +20,12 @@
 							{{item.likesCount}}
 						</v-card-text>
 
-						<!-- v-if="!item.inCart" -->
+						 v-if="!item.inCart"
 						<v-card-actions></v-card-actions>
 					</v-card>
 				</v-flex>
 			</v-layout>
-		</v-container>
+		</v-container>-->
 	</div>
 </template>
 
