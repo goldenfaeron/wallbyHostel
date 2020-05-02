16b5b201766df3ba03070193cae07040a07c95ee
Samuel Austen <furneauxsam1994@gmail.com>
2020-05-02 22:06:14 +0200

    right drawer on mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -21,29 +21,67 @@
 			</v-list>
 		</v-navigation-drawer>
 		<v-app-bar :clipped-left="clipped" app fixed color="accent lighten-3 " type>
-			<v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+			<!-- Mobile display left -->
+			<v-icon
+				color="accent"
+				class="hidden-sm-and-up"
+				@click.stop="drawer = !drawer"
+				style="margin-left:10px"
+			>mdi-heart-multiple</v-icon>
 
+			<v-app-bar-nav-icon
+				class="hidden-xs-only"
+				color="accent darken-2"
+				@click.stop="drawer = !drawer"
+			/>
 			<v-toolbar-title style="cursor:pointer" @click="go('/')">
 				<b>{{title}}</b>
 			</v-toolbar-title>
-			<v-icon color="accent" @click="go('/')" style="margin-left:10px">mdi-heart-multiple</v-icon>
+			<v-icon
+				color="accent"
+				class="hidden-xs-only"
+				@click.stop="drawer = !drawer"
+				style="margin-left:10px"
+			>mdi-heart-multiple</v-icon>
 
 			<v-toolbar-title
 				class="ml-2 grey--text hidden-xs-only"
 			>Find what you need in {{$store.state.city}}, Albania</v-toolbar-title>
 			<v-spacer />
 
-			<v-btn icon @click.stop="rightDrawer = !rightDrawer">
-				<v-icon>mdi-account</v-icon>
+			<v-btn
+				small
+				fab
+				flat
+				class="hidden-sm-and-up"
+				color="accent lighten-3"
+				@click.stop="rightDrawer = !rightDrawer"
+			>
+				<v-icon color="accent darken-2">mdi-menu</v-icon>
 			</v-btn>
+			<!-- Mobile display right -->
+
+			<!-- <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+				<v-icon>mdi-account</v-icon>
+			</v-btn>-->
 		</v-app-bar>
 		<v-content>
 			<!-- <v-container> -->
 			<nuxt />
 			<!-- </v-container> -->
 		</v-content>
-		<v-navigation-drawer width="500" v-model="rightDrawer" :right="right" temporary fixed>
-			<DrawerAnnouncement></DrawerAnnouncement>
+		<v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+			<v-list>
+				<v-list-item v-for="(item, i) in nav" :key="i" :to="item.to" router exact>
+					<v-list-item-action>
+						<v-icon color="primary" v-if="item.icon">{{ item.icon }}</v-icon>
+						<v-img height="32" width="24" v-else :src="item.img"></v-img>
+					</v-list-item-action>
+					<v-list-item-content>
+						<v-list-item-title v-text="item.title" />
+					</v-list-item-content>
+				</v-list-item>
+			</v-list>
 		</v-navigation-drawer>
 		<Footer></Footer>
 		<GoBackButton></GoBackButton>
