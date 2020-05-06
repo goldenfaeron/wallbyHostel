a9e5105d67f58c2fcdaa2b81552f2ed133ee4c97
Samuel Austen <furneauxsam1994@gmail.com>
2020-05-06 23:31:39 +0200

    no message

--- pages/activities/_id.vue
+++ pages/activities/_id.vue
@@ -13,9 +13,23 @@
 					</v-img>
 					<h2 primary-title>{{thing.location_string}}</h2>
 					<p>{{thing.description}}</p>
+					<!-- Featured business -->
+					<h2>
+						What to do on {{thing.name}}
+						<v-icon>mdi-account-star</v-icon>
+					</h2>
+
+					<v-divider></v-divider>
+					<v-container grid-list-lg>
+						<v-layout row wrap>
+							<v-flex xs6 md4 v-for="(item, index) in business" :key="index">
+								<CardFeaturedHotel :props="item"></CardFeaturedHotel>
+							</v-flex>
+						</v-layout>
+					</v-container>
 
 					<h2>
-						Reviews
+						Reviews of {{thing.name}} from Tripadvisor
 						<v-icon>mdi-account-star</v-icon>
 						<v-divider></v-divider>
 					</h2>
@@ -57,6 +71,8 @@
 			</h2>
 			<googleMapCoordinate :props="{Lat: thing.latitude, Lng: thing.longitude}"></googleMapCoordinate>
 		</v-container>
+
+		<!-- {{business}} -->
 	</div>
 </template>
 
@@ -64,6 +80,7 @@
 export default {
 	async asyncData({ $axios, route, store }) {
 		let collection = "tripadvisor_thingstodo_borsh";
+		let collection2 = "shops_featured";
 
 		let request1 = await $axios.post(
 			store.state.webRoot +
@@ -74,22 +91,24 @@ export default {
 			{ filter: { slug: route.params.id } }
 		);
 
-		// let request2 = await $axios.post(
-		// 	store.state.webRoot +
-		// 		"/api/collections/get/" +
-		// 		collection +
-		// 		"?token=" +
-		// 		store.state.collectionsToken,
-		// 	{ limit: 5, sort: { _created: -1 } }
-		// );
+		let request2 = await $axios.post(
+			store.state.webRoot +
+				"/api/collections/get/" +
+				collection2 +
+				"?token=" +
+				store.state.collectionsToken,
+			{ limit: 5, sort: { _created: -1 }, populate: 1 }
+		);
 		return {
-			thing: request1.data.entries[0]
+			thing: request1.data.entries[0],
+			business: request2.data.entries
 		};
 	},
 
 	components: {
 		CardReview: () => import("@/components/CardReview"),
 		CardInstagram: () => import("@/components/CardInstagram"),
+		CardFeaturedHotel: () => import("@/components/CardFeaturedHotel"),
 		googleMapCoordinate: () => import("@/components/googleMapCoordinate")
 	},
 
---
+++ pages/featured_business/_id.vue
@@ -0,0 +1,135 @@
+<template>
+	<div>
+		<SplashTitle :props="shop"></SplashTitle>
+		<v-container grid-list-xs>
+			<v-layout row wrap>
+				<v-flex xs12 md6>
+					<h1>{{shop.name}}</h1>
+					<!-- {{shop.linked_object}} -->
+					<p v-html="shop.description"></p>
+					<v-divider></v-divider>
+				</v-flex>
+				<v-flex xs12 md6>
+					<ImageGallery :props="shop.gallery"></ImageGallery>
+				</v-flex>
+
+				<v-flex xs12 my-5>
+					<v-container grid-list-lg>
+						<v-layout row wrap>
+							<v-flex xs12 sm6 md4 v-for="(room, index) in shop.offers" :key="index">
+								<CardRoom :details="room.value" :show="false"></CardRoom>
+							</v-flex>
+						</v-layout>
+					</v-container>
+				</v-flex>
+			</v-layout>
+		</v-container>
+
+		<v-container grid-list-lg>
+			<h2>{{shop.name}} Offers</h2>
+			{{shop.telephone}}
+			<a
+				:href="'https://wa.me/'+shop.telephone"
+			>Message or call {{shop.owner}} on Whatsapp</a>
+			<v-layout row wrap mt-5>
+				<v-flex xs12 sm6 md4 v-for="(item, index) in shop.linked_shops" :key="index">
+					<CardBar :props="item"></CardBar>
+				</v-flex>
+			</v-layout>
+		</v-container>
+		<v-container grid-list-lg>
+			<h2>{{shop.owner}}'sother businesses</h2>
+			<v-layout row wrap mt-5>
+				<v-flex xs12 sm6 md4 v-for="(item, index) in shop.linked_shops" :key="index">
+					<CardBar :props="item"></CardBar>
+				</v-flex>
+			</v-layout>
+		</v-container>
+		<v-container grid-list-lg>
+			<h2>{{shop.owner}}'s hotels</h2>
+			{{shop.owner}} has {{shop.linked_hotels.length}} hotel
+			<span v-if="shop.linked_hotels.length >1">s</span>
+			in {{$store.state.city}}
+			<v-layout row wrap mt-5>
+				<v-flex xs12 sm6 md4 v-for="(item, index) in shop.linked_hotels" :key="index">
+					<CardHotel :props="item"></CardHotel>
+				</v-flex>
+			</v-layout>
+		</v-container>
+		<v-container grid-list-lg>
+			<h2>{{shop.name}}other hotels</h2>
+			{{shop.owner}} has {{shop.linked_featured_hotels.length}} featured hotel
+			<span
+				v-if="shop.linked_featured_hotels.length >1"
+			>s</span>
+			in {{$store.state.city}}
+			<v-layout row wrap mt-5>
+				<v-flex xs12 sm6 md4 v-for="(item, index) in shop.linked_featured_hotels" :key="index">
+					<CardFeaturedHotel :props="item"></CardFeaturedHotel>
+				</v-flex>
+			</v-layout>
+		</v-container>
+	</div>
+</template>
+
+<script>
+export default {
+	async asyncData({ $axios, route, store }) {
+		let collection = "shops_featured";
+
+		let request1 = await $axios.post(
+			store.state.webRoot +
+				"/api/collections/get/" +
+				collection +
+				"?token=" +
+				store.state.collectionsToken,
+			{ filter: { slug: route.params.id }, populate: 1 }
+		);
+
+		// let request2 = await $axios.post(
+		// 	store.state.webRoot +
+		// 		"/api/collections/get/" +
+		// 		collection +
+		// 		"?token=" +
+		// 		store.state.collectionsToken,
+		// 	{ limit: 5, sort: { _created: -1 } }
+		// );
+		return {
+			// shop: request1.data.entries[0],
+			shop: request1.data.entries[0]
+		};
+	},
+	components: {
+		CallToAction: () => import("@/components/CallToAction"),
+		CommentsParallax: () => import("@/components/CommentsParallax"),
+		JumbotronGradient: () => import("@/components/JumbotronGradient"),
+		ImageGallery: () => import("@/components/ImageGallery"),
+		SplashTitle: () => import("@/components/SplashTitle"),
+		CardRoom: () => import("@/components/CardRoom"),
+		CardBar: () => import("@/components/CardBar"),
+		CardHotel: () => import("@/components/CardHotel"),
+		CardFeaturedHotel: () => import("@/components/CardFeaturedHotel"),
+
+		CertificateCorona: () => import("@/components/CertificateCorona"),
+		CertificateCService: () => import("@/components/CertificateCService"),
+		CertificateQuarantine: () => import("@/components/CertificateQuarantine")
+	},
+	data() {
+		return {};
+	},
+
+	head() {
+		return {
+			title: this.shop.name,
+			meta: [
+				{
+					hid: "shop in " + this.$store.state.city,
+					name: this.shop.name,
+					content: this.shop.description.substr(0, 100)
+				}
+			]
+		};
+	}
+};
+</script>
+
