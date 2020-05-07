4096039b32492d1d3ffd04d0b1ad6ed74ce1d81b
Goldenfaeron <tobias@mreich.de>
2020-05-07 17:23:57 +0200

    all youtube fixes

--- pages/youtube/_id.vue
+++ pages/youtube/_id.vue
@@ -9,9 +9,15 @@
 			width="100%"
 		></iframe>
 		<br />
+		<v-layout row wrap justify-space-around="">
+			
+			<v-flex lg11>
+
 		<p class="headline font-weight-bold">{{video.name}}</p>
 
-		<v-layout justify-space-between>
+		
+				<v-layout justify-space-between="">
+					
 			<p>{{video.viewCount}} Views &#183; {{video.date.slice(8,10)}}.{{video.date.slice(5,7)}}.{{video.date.slice(0,4)}}</p>
 			<p>
 				<v-icon>mdi-thumb-up</v-icon>
@@ -19,31 +25,33 @@
 				<v-icon>mdi-thumb-down</v-icon>
 				{{video.dislikes}}
 			</p>
+				</v-layout>
+			</v-flex>
 		</v-layout>
+	
 		<br />
 		<v-divider></v-divider>
 		<br />
 		<v-container grid-list-lg>
 			<v-layout row wrap>
-				<v-flex xs12 sm12 md6 lg4 v-for="(item, index) in videos.slice(0,6)" :key="index">
-					<v-card flat @click.native="dialog = true; arrNumber = index">
-						<v-layout row wrap>
-							<v-flex lg2>
-								<v-img src></v-img>
-							</v-flex>
-							<v-flex lg10>
+				<v-flex xs12 sm12 md6 lg3 v-for="(item, index) in videos.slice(0,8)" :key="index">
+					<v-hover>
+						<v-card
+							flat
+							@click.native="dialog = true; arrNumber = index"
+							slot-scope="{ hover }"
+							:class="`elevation-${hover ? 12 : 0}`"
+						>
+							<nuxt-link to style="text-decoration:none;">
 								<v-img :src="'https://i.ytimg.com/vi/'+item.id+'/maxresdefault.jpg'" height="200px"></v-img>
-
 								<p
 									class="font-weight-bold"
-									style="height: 2.8rem; line-height: 1.4rem; overflow: hidden;"
+									style="height: 2.8rem; line-height: 1.5rem; overflow: hidden;"
 								>{{item.name}}</p>
-								{{item.channelName}}
-								<br />
 								{{item.viewCount}} Views &#183; {{item.likes}} likes
-							</v-flex>
-						</v-layout>
-					</v-card>
+							</nuxt-link>
+						</v-card>
+					</v-hover>
 				</v-flex>
 			</v-layout>
 		</v-container>
@@ -56,15 +64,23 @@
 		>
 			<v-card>
 				<v-layout justify-space-between>
-					<div @click="reloadPage()">
+					<div>
 						<v-btn icon @click="dialog=false;" color="red">
 							<v-icon>mdi-close</v-icon>
 						</v-btn>
 					</div>
-					<v-btn color="red" :href="videos[this.arrNumber].url">Watch on YouTube</v-btn>
+					<v-tooltip top>
+						<template v-slot:activator="{ on }">
+							<a target="_blank" :href="videos[arrNumber].url" v-on="on">
+								<v-img height="32" width="32" src="/img/link.svg"></v-img>
+							</a>
+						</template>
+						<span>Watch on Youtube</span>
+					</v-tooltip>
 				</v-layout>
 
 				<iframe
+					v-if="dialog"
 					:src="'https://www.youtube.com/embed/'+videos[this.arrNumber].id"
 					frameborder="0"
 					allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
@@ -115,13 +131,12 @@ export default {
 			x: 0,
 			y: 0
 		},
-		arrNumber: "0",
-	//	name: this.video.name,
-	//	description: this.video.details,
-	//	thumbnailUrl: 'https://i.ytimg.com/vi/'+this.video.id+'/maxresdefault.jpg',
-	//	uploadDate: this.video.date,
-	//	contentUrl: this.video.url
-
+		arrNumber: "0"
+		//	name: this.video.name,
+		//	description: this.video.details,
+		//	thumbnailUrl: 'https://i.ytimg.com/vi/'+this.video.id+'/maxresdefault.jpg',
+		//	uploadDate: this.video.date,
+		//	contentUrl: this.video.url
 	}),
 
 	mounted() {
@@ -142,10 +157,11 @@ export default {
 			"@type": "VideoObject",
 			name: this.video.name,
 			description: this.video.details,
-			thumbnailUrl: 'https://i.ytimg.com/vi/'+this.video.id+'/maxresdefault.jpg',
+			thumbnailUrl:
+				"https://i.ytimg.com/vi/" + this.video.id + "/maxresdefault.jpg",
 			uploadDate: this.video.date,
 			contentUrl: this.video.url,
-			embedUrl: this.video.url,
+			embedUrl: this.video.url
 		};
 	}
 };
