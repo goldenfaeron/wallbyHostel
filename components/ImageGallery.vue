7ecb42a6103b27c62c19dc878c8394126843499f
Samuel Austen <furneauxsam1994@gmail.com>
2020-05-11 18:11:07 +0200

    gallery component

--- components/ImageGallery.vue
+++ components/ImageGallery.vue
@@ -1,15 +1,15 @@
 <template>
-	<v-card>
-		<v-container grid-list-sm fluid>
-			<v-layout row wrap>
-				<v-flex v-for="image in props" :key="image" xs6 md4 d-flex>
-					<v-card flat tile class="d-flex">
+	<v-container grid-list-sm fluid>
+		<v-layout row wrap>
+			<v-flex xs12 md6 order-xs2>
+				<v-layout row wrap>
+					<v-flex v-for="image in props" :key="image" xs1 sm2 md4 @click="displayImage = image.path">
 						<v-img
+							style="cursor:pointer"
 							:lazy-src="$store.state.webRoot+'/api/cockpit/image?token='+$store.state.imageToken+'&src='+image._id+'&w=200&h=200&o=true'"
 							:src="$store.state.webRoot + image.path"
 							class="grey lighten-2"
-							height="200"
-							width="200"
+							:height="[$vuetify.breakpoint.smAndUp ? '100' : '200']"
 						>
 							<template v-slot:placeholder>
 								<v-layout fill-height align-center justify-center ma-0>
@@ -17,15 +17,26 @@
 								</v-layout>
 							</template>
 						</v-img>
-					</v-card>
-				</v-flex>
-			</v-layout>
-		</v-container>
-	</v-card>
+					</v-flex>
+				</v-layout>
+			</v-flex>
+			<v-flex xs12 md6 order-xs1 order-md2>
+				<v-img
+					:height="[$vuetify.breakpoint.smAndUp ? '400px' : '200px']"
+					:src="$store.state.webRoot + displayImage"
+				></v-img>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
 <script>
 export default {
+	data() {
+		return {
+			displayImage: this.props[0].path
+		};
+	},
 	props: ["props"]
 };
 </script>
