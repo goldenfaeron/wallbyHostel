6a30d9125940b92f3f745702e2c75c1e997bf6cd
Samuel Austen <furneauxsam1994@gmail.com>
2020-05-28 21:19:52 +0200

    route params

--- middleware/api.js
+++ middleware/api.js
@@ -1,7 +1,8 @@
-export default function ({ store, $axios }) {
+export default function ({ store, $axios, route }) {
     return $axios.$get('https://cockpit.goborshi.hackmylanguage.com/api/collections/get/airbnb?token=641a6e0c88f94f7d2adadd184752e1'
     ).then((res) => {
         console.log(store.state.city)
+        console.log(route.params)
         // console.log(res)
         store.commit("setPageData", res.entries[1])
     });
