652541cfeab825e63659534fea055ac058a5ee9c
Goldenfaeron <tobias@mreich.de>
2020-06-17 18:32:12 +0200

    no message

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,7 @@
 	<div>
 		<v-layout row wrap>
 			<v-flex lg6>
-				<div class="logo">
-
-				</div>
+				<div class="logo"></div>
 
 				<div class="welcome box vcenter eq matched pinged loaded">
 					<v-img
@@ -19,10 +17,21 @@
 	</div>
 </template>
 
-<style lang="css">
+<style >
 .logo {
-	background: url(https://www.kexhostel.is/media/svg-icon/kex-big.svg) no-repeat center center;
-	height: 200px;
-	width: 200px;
+	background-color: #6db3f2;
+	display: block;
+	margin: auto;
+	background: url(https://www.kexhostel.is/media/svg-icon/kex-big.svg) no-repeat
+		center;
+	height: 50%;
+	width: 50%;
 }
 </style>
+<script>
+export default {
+	theme: {
+		dark: true
+	}
+};
+</script>
