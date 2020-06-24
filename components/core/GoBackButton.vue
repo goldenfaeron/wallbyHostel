1c019e8e09dd7fddfd1993a3b69ba952d3a6bdd5
Goldenfaeron <tobias@mreich.de>
2020-06-24 11:46:41 +0200

    no message

--- components/core/GoBackButton.vue
+++ components/core/GoBackButton.vue
@@ -1,15 +1,21 @@
 <template>
-
 	<v-flex>
 		<v-btn @click="menu= !menu" small fixed top text right fab v-show="scrollButton">
 			<v-icon color="grey" x-large>mdi-menu</v-icon>
 		</v-btn>
 		<v-container grid-list-lg v-show="menu" fluid :style="'height:' +windowSize.y+'px;'">
 			<v-layout column align-center justify-center wrap>
-
-					<v-flex v-for="(item, index) in nav" :key="index" v-if="index < 9" xs12> <nuxt-link :to="item.to"><div class="display-3 secondary--text" >{{item.title}}</div></nuxt-link></v-flex>
-
-
+				<v-flex v-for="(item, index) in nav" :key="index" v-if="index < 9" xs12>
+					<nuxt-link :to="item.to">
+						<v-hover>
+							<div
+								slot-scope="{ hover }"
+								@click="menu= false"
+								:class="`${hover ? 'white' : 'secondary'}--text text--lighten-3 font-weight-bold display-3`"
+							>{{item.title}}</div>
+						</v-hover>
+					</nuxt-link>
+				</v-flex>
 			</v-layout>
 		</v-container>
 	</v-flex>
@@ -48,10 +54,18 @@ export default {
 		// 		behavior: "smooth"
 		// 	});
 		// },
-	},
-	
+	}
 };
 </script>
-<style lang="">
-
+<style lang="css">
+.hover {
+	text-decoration: none;
+}
+.hover.ex1 {
+	color: black;
+}
+.hover.ex1:hover,
+.hover.ex1:active {
+	color: white;
+}
 </style>
