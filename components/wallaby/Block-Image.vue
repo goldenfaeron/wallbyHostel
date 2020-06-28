05cf580f9d0483265a6b83ad682f8f3dc797e30a
DESKTOP-5G1L3OM\Loch <dummy@dummy.com>
2020-06-28 22:05:30 +1000

    About with components

---
+++ components/wallaby/Block-Image.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+  
+    	<v-container fluid fill-height class="ma-0 pa-0">
+			<v-layout row wrap>
+				<v-flex lg6>
+					<div style="background-color: #000000;">
+						<v-container
+							
+						>
+							<v-container fill-height>
+								<v-layout align-center justify-center row fill-height>
+									<v-layout column cols="12" tag="h1" pl-2>
+										<div class="text">
+										<p
+												:class="[$vuetify.breakpoint.smAndDown ? 'display-1' : 'display-3']  + ' font-weight-bold'"
+											>{{props.block1_title}}</p>
+											<div
+												v-html="props.block1_text"
+												:class="[$vuetify.breakpoint.smAndDown ? 'headline' : 'display-1']  + ' font-weight-bold'"
+											></div>
+										</div>
+									</v-layout>
+								</v-layout>
+							</v-container>
+						</v-container>
+					</div>
+				</v-flex>
+				<v-flex lg6>
+					<v-img height="100%" :src="asset + props.image2.path">
+					</v-img>
+				</v-flex>
+			</v-layout>
+		</v-container>
+    </div>
+</template>
+
+<script>
+
+import { Mixin } from "~/mixins/windowSize.js";
+
+export default {
+	mixins: [Mixin],
+    props: ['props'],
+}
+</script>
